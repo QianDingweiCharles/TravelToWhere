e69b66c441e84375756f80e91ad6bce19d21266a
QianDingwei <qdw1995@126.com>
2019-07-28 07:31:21 +0800

    增加city页面header

---
+++ src/pages/City/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <router-link to="/">
+      <div class="header">
+        城市选择
+        <div class="iconfont header-back">&#xe624;</div>
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang='scss' scoped>
+@import '~styles/varibles.scss';
+.header {
+  overflow: hidden;
+  height: $headerHeight;
+  line-height: $headerHeight;
+  text-align: center;
+  color: #fff;
+  background-color: $bgColor;
+  position: relative;
+  .header-back {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0.64rem;
+    text-align: center;
+    font-size: 0.32rem;
+    color: #fff;
+  }
+}
+</style>
