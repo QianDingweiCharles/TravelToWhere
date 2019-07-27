dbac34b6ad130f077a0d5a1090d66d7c05fb28e1
QianDingwei <qdw1995@126.com>
2019-07-27 16:04:49 +0800

    增加字体图标

--- src/pages/Home/components/Header.vue
+++ src/pages/Home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">输入城市/游玩/景点/主题</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/游玩/景点/主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
   </div>
 </template>
 
@@ -17,24 +25,35 @@ export default {
   display: flex;
   background-color: #00bcd4;
   line-height: 0.86rem;
+  color: #fff;
   .header-left {
     width: 0.64rem;
     float: left;
+    .back-icon {
+      text-align: center;
+      font-size: 0.24;
+    }
   }
   .header-input {
     flex: 1;
     height: 0.64rem;
     line-height: 0.64rem;
+    padding-left: 0.2rem;
     margin-left: 0.2rem;
     margin-top: 0.12rem;
     background-color: #fff;
     border-radius: 0.1rem;
+    text-align: left;
     color: #ccc;
   }
   .header-right {
     width: 1.24rem;
     float: right;
     text-align: center;
+    .arrow-icon {
+      font-size: 0.24rem;
+      margin-left: -0.04rem;
+    }
   }
 }
 </style>
