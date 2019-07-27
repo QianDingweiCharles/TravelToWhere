cf0ff64b344ae19aa3333b55bb344b1089a97637
QianDingwei <qdw1995@126.com>
2019-07-27 15:10:17 +0800

    项目构建完成

---
+++ src/pages/Home/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header">
+    <div class="header-left">header-left</div>
+    <div class="header-input">header-input</div>
+    <div class="header-right">header-right</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style>
+</style>
