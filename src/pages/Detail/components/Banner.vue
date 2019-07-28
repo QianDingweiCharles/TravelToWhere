e55d96012daf7998aa5e1d7153442c66bdcd9898
QianDingwei <qdw1995@126.com>
2019-07-28 14:31:53 +0800

    详情页面banner架构搭建

---
+++ src/pages/Detail/components/Banner.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>DetailBanner</div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang='scss' scoped>
+@import '~styles/varibles.scss';
+@import '~styles/mixins.scss';
+</style>
