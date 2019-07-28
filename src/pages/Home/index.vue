e0ff5c39a2c3cac56f7d1a6f8eafa498ca409eb8
QianDingwei <qdw1995@126.com>
2019-07-28 13:01:58 +0800

    完成路由跳转发送axios请求优化

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 
 export default {
   name: 'Home',
@@ -27,7 +28,7 @@ export default {
   },
   data() {
     return {
-      city: '',
+      lastCity: '',
       swiperList: [],
       icons: [],
       recommendList: [],
@@ -36,7 +37,8 @@ export default {
   },
   methods: {
     getHomeInfo() {
-      axios.get('/api/index.json').then(this.getHomeInfoSucc)
+      // axios.get(`/api/index.json?city=${this.city}`).then(this.getHomeInfoSucc)
+      axios.get('/api/index.json?city=' + this.city).then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc(res) {
       res = res.data
@@ -50,8 +52,18 @@ export default {
       }
     }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   mounted() {
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  activated() {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
