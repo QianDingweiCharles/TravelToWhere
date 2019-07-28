0616bd40845ab82a245102452ea97a0c1946cc33
QianDingwei <qdw1995@126.com>
2019-07-28 09:03:35 +0800

    增加右侧字母滚动栏

--- src/pages/City/index.vue
+++ src/pages/City/index.vue
@@ -2,18 +2,24 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alphabel></city-alphabel>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabel from './components/Alphabel'
 
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlphabel
   }
 }
 </script>
