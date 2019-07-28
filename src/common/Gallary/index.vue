1bf8a8b7ca5b4e7ff08b6c3e59f087d521651d66
QianDingwei <qdw1995@126.com>
2019-07-28 18:42:51 +0800

    完成画廊组件

---
+++ src/common/Gallary/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container" @click="handleGallyClick">
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+        <!-- slides -->
+        <swiper-slide v-for="(item,index) in imgs" :key="index">
+          <img class="gally-img" :src="item" />
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Gallary',
+  props: {
+    imgs: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observeParents: true,
+        observer: true
+      }
+    }
+  },
+  methods: {
+    handleGallyClick() {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  z-index: 99;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background-color: #000;
+  .wrapper {
+    height: 0;
+    width: 100%;
+    padding-bottom: 100%;
+    .gally-img {
+      width: 100%;
+    }
+    .swiper-pagination {
+      color: #fff;
+      bottom: 0rem;
+    }
+  }
+}
+</style>
