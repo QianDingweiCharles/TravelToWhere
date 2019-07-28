f6a159e5456a27a6594a735f314e3477b766d354
QianDingwei <qdw1995@126.com>
2019-07-28 18:47:26 +0800

    完成DetailList架构搭建

---
+++ src/pages/Detail/components/List.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>List</div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList'
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/pages/Detail/index.vue
+++ src/pages/Detail/index.vue
@@ -2,18 +2,21 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
+    <detail-list></detail-list>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
   }
 }
 </script>
